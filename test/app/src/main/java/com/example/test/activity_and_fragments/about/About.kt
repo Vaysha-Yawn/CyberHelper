a905d92953eb93a6e04b9c29b0ae0341672bda87
Vaysha-Yawn <90606158+Vaysha-Yawn@users.noreply.github.com>
2022-03-21 14:44:28 +0700

    к

--- test/app/src/main/java/com/example/test/activity_and_fragments/about/About.kt
+++ test/app/src/main/java/com/example/test/activity_and_fragments/about/About.kt
@@ -15,15 +15,10 @@ class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.about)
-        val goalSpinner = findViewById<Spinner>(R.id.goalSpinner)
-        val arr = arrayOf("Одна цель", "Несколько целей")
-        goalSpinner.adapter = ArrayAdapter(this, R.layout.spinner_item, arr)
-        goalSpinner.setPopupBackgroundDrawable(resources.getDrawable(R.drawable.bcg_blue_line))
-        //goalSpinner
+
         /*} catch (e: Exception) {
             Toast.makeText(this, "$e", Toast.LENGTH_LONG).show()
-        }
-*/
+        } */
     }
 }
 
